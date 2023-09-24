55e2f61c98014bd39ae578504cc0641a6faaa4df
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-09-25 02:44:35 +0600

    Add forLoop.cpp Illustrate for loop in C++

---
+++ Loops/forLoop.cpp
@@ -0,0 +1,21 @@
+// for loop is used when we know the exact number of iterations
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // for (initialization; condition; update) {
+        // body   
+    // }
+
+    // initialization - initialize a variable
+    // condition - if true the loop is executed
+    // update - increment or decrement of initialize variable
+
+    for(int i=0; i<10; i++) {
+        cout<< i<< "  Hello, World!"<< endl;
+    }
+
+    return 0;
+}
